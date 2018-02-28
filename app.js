2658e14ebd4a56b923a9c6f52e13204163a53eff
noreenm <noreenmadden@Noreens-MacBook-Pro.local>
2018-02-27 19:33:43 -0600

    Fixed typo for port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 var app = express();
 
 //  this is to make sure it can work on local or remote server - bc we don't know what port it'll be on
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
